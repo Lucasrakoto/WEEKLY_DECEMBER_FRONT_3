49b8c3e7b5d28a2d4c64a3312792a5a87f3c44ea
Lucasrakoto <lucasrakotoarihanta839@gmail.com>
2023-12-17 21:04:49 +0100

    dernier commit avant deployement

---
+++ main.js
@@ -0,0 +1,13 @@
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const slide = document.querySelector(".slide__list");
+// let scrollP = 0;
+prevButton.onclick = () => {
+  //   slide.scrollLeft -= 200;
+};
+nextButton.onclick = () => {
+  //   slide.scrollLeft += 200;
+  //   scrollP += 20 + "%";
+  //   alert("+50%");
+  //   slide.setProperty("--scrollPage", "-" + 50 + "%");
+};
